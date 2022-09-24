26f96480eedf20841bdc390078561fe90f98e359
rewls <rew0316@gmail.com>
2022-09-25 07:09:00 +0900

    Change item styles

--- public/js/readdata.js
+++ public/js/readdata.js
@@ -40,13 +40,16 @@ function setdata() {
               <a href="https://exco.co.kr/schedule/schedule_view.html?code=P_AGPV56306&ex_cate=1">
                 <div class="card shadow-sm zoom">
                   <img class="bd-placeholder-img card-img-top" src="${data.val().ImgUrl}" width="100%">
-                  <div class="card-body">
+                  <div class="card-body p-6">
                     <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group btn-group-sm btn-border-radius-sm" role="group" aria-label="status">
+                      <div class="condition btn-group btn-group-sm btn-border-radius-sm" role="group" aria-label="condition">
                         <button type="button" class="btn btn-danger" style="--bs-btn-border-radius: .7rem;">진행중</button>
                       </div>
+                      <div class="small text-muted text-end">
+                        <date>${data.val().Start_date}</date>
+                        ~&nbsp;<date>${data.val().End_date}</date>
+                      </div>
                     </div>
-                    <date class="text-muted">${data.val().Start_date} ~ ${data.val().End_date}</date>
                     <div class="fs-6 text-muted pt-3">${data.val().InputOrganizer}</div>
                     <p class="card-text fs-5 fw-bold">${data.val().InputName}</p>
                   </div>
