8a58373f2e563d1b6debf86918e84ca87b60890d
hyonso <gysth0106@gmail.com>
2022-09-25 04:39:52 +0900

    add data

--- public/js/readdata.js
+++ public/js/readdata.js
@@ -1,6 +1,8 @@
 document.write('<script src="https://www.gstatic.com/firebasejs/8.6.5/firebase.js"></script>');
 
 function init_DB(){
+
+  
   const firebaseConfig = {
     apiKey: "AIzaSyANW9B69OXwB3MuMysbppB6we8uI4l-2XI",
     authDomain: "runners-5383d.firebaseapp.com",
@@ -11,9 +13,10 @@ function init_DB(){
     appId: "1:61987349638:web:41d7cb172782c2662fdd07",
     measurementId: "G-DC3RLVG1K6"
   };
-
+  
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+
 }
 
 
@@ -21,6 +24,7 @@ function init_DB(){
 function setdata() {
 
   //alert("전송 완료");
+
 
   let database = firebase.database();
   const DB = database.ref('Event/').orderByChild("End_date");
@@ -49,7 +53,12 @@ function setdata() {
             </li>
             `;
     $('.col1').before(templete)
+
+    
 })
+
+
+    
 
   /*
   var dbTestRef = firebase.database().ref('Event/')
@@ -78,5 +87,3 @@ function setdata() {
 })*/
 
 }
-
-setdata();
