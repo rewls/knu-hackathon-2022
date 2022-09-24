394fe9cc09d44556225ddea15a12909ace39f83d
rewls <rew0316@gmail.com>
2022-09-24 20:18:07 +0900

    Include javascript modules in main.js

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+import "filter.js";
+import "adddata.js";
+import "readdata.js";
+
+// firebase
+import "/__/firebase/9.10.0/firebase-app-compat.js"
+import "/__/firebase/9.10.0/firebase-auth-compat.js"
+import "/__/firebase/9.10.0/firebase-firestore-compat.js"
+import "/__/firebase/9.10.0/firebase-storage-compat.js"
