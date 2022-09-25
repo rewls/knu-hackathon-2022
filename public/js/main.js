193afc35b365b63ae56ab07c22265da1ff117cfa
rewls <rew0316@gmail.com>
2022-09-25 16:44:30 +0900

    Add connection between event filter and status filter

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 document.write("<script src='./js/adddata.js'></script>")
-document.write("<script defer src='./js/eventFilter.js'></script>")
 document.write("<script src='./js/readdata.js'></script>")
 document.write("<script defer src='./js/search.js'></script>")
+document.write("<script defer src='./js/eventFilter.js'></script>")
 document.write("<script defer src='./js/statusFilter.js'></script>")
